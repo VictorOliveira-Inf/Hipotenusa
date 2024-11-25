fcb7d5d2e4d536748240659c344571c589b3724b
STAYBIGFLOPPA <victor.oliveiraconta@gmail.com>
2024-11-25 15:02:07 -0300

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+
+public class JavaApplication2 {
+
+    
+    public static void main(String[] args) 
+    {/*
+      int x = 10;
+      int y = 5;
+      int resultado;
+      
+      
+      
+      
+      /*System.out.println("Valor de X: " + x);
+      System.out.println("Valor de Y: " + y);
+      
+       System.out.println("Resultado da soma: " + (resultado = x + y));
+      System.out.println("Resultado da subtracao: " + (resultado = x - y));
+      System.out.println("Resultado da divisao: " + (resultado = x / y));
+      System.out.println("Resultado da multiplicacao: " + (resultado = x * y));
+      System.out.println("Resultado do resto da divisao: " + (resultado = x % y));
+    
+         
+         int b = 10;
+         int c = 6;
+         int a = 8;
+         int hipo;
+         int cat;
+         
+         hipo = a * a;
+         cat = (b*b)+(c*c);
+         
+         
+         
+         System.out.println("o resutaldo da hipotenusa: " + hipo);
+         System.out.println("o resutaldo do cateto: " + cat);
+  
+        
+        float a = 5, b = 10 ;
+        
+        float res;
+        
+        res = (a * b) / 2;
+        
+        System.out.println("resultado" + res);
+       
+        
+        double pi = 3.14;  
+       double area;
+        double r = 5;
+        
+        area = (r*r) * pi;
+        
+        System.out.println("resultado" + area);
+ */
+        double delta;
+        double b = 5, c = 4, a = 3;
+        
+        delta = (b * b) - (4*a*c) ;
+        
+         System.out.println("resultado" + delta);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+      
+    }
+    
+}
